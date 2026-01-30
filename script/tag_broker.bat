92da858ea9fde9b5c06cfb43accde4e687c12c52
Bobo <yanglinbo@gmail.com>
2026-01-30 12:30:57 +0800

    chore: enhance tracing by adding context propagation for producer and consumer spans

--- script/tag_broker.bat
+++ script/tag_broker.bat
@@ -1,6 +1,6 @@
 git tag broker/v1.3.2 --force
 
-git tag broker/kafka/v1.3.3 --force
+git tag broker/kafka/v1.3.4 --force
 git tag broker/mqtt/v1.3.2 --force
 git tag broker/nats/v1.3.2 --force
 git tag broker/nsq/v1.3.2 --force
